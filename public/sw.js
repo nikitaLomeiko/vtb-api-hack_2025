8e4c3cb198d0fc7278cd32975d64bb9ffffd7bd1
Nikita <nikitalomieiko@gmail.com>
2025-11-04 16:36:49 +0300

    init service worker

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = ["/", "/static/js/bundle.js", "/static/css/main.css", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Возвращаем кэшированную версию или делаем запрос
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,15 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <App />
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
