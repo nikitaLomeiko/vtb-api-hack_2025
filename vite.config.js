2f73cbc69a520c74c71a925543aa62d134d0e2df
NikitaTorbenko <2015nekitciti@gmail.com>
2025-11-12 21:36:00 +0300

    fix: alias

--- vite.config.js
+++ vite.config.js
@@ -33,15 +33,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': new URL('./src', import.meta.url).pathname,
-      '@styles': new URL('./src/styles', import.meta.url).pathname,
-      '@components': new URL('./src/components', import.meta.url).pathname,
-      '@hooks': new URL('./src/hooks', import.meta.url).pathname,
       '@pages': new URL('./src/pages', import.meta.url).pathname,
-      '@assets': new URL('./src/assets', import.meta.url).pathname,
-      '@routes': new URL('./src/routes', import.meta.url).pathname,
-      '@store': new URL('./src/store', import.meta.url).pathname,
-      '@lib': new URL('./src/lib', import.meta.url).pathname,
-      '@api': new URL('./src/api', import.meta.url).pathname,
       '@modules': new URL('./src/modules', import.meta.url).pathname,
       '@shared': new URL('./src/shared', import.meta.url).pathname,
     },
